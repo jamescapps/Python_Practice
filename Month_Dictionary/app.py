804e39280b012c8c902c80f40f48a657f7f39178
James Capps <jcapps@protonmail.com>
2020-03-05 10:29:56 -0600

    Less lines.

--- Month_Dictionary/app.py
+++ Month_Dictionary/app.py
@@ -1,25 +1,11 @@
 # User can find out how many days in a month.
 
-
 month = (input('Input 3 letter month abbreviation: ')).lower()
+month_dict = {'jan': 31, 'feb': 28, 'mar': 31, 'apr': 30, 'may': 31, 'jun': 30, 'jul': 31, 'aug': 31, 'sep': 30,
+              'oct': 31, 'nov': 30, 'dec': 31}
 
 
 def num_days(item):
-    month_dict = {
-        'jan': 31,
-        'feb': 28,
-        'mar': 31,
-        'apr': 30,
-        'may': 31,
-        'jun': 30,
-        'jul': 31,
-        'aug': 31,
-        'sep': 30,
-        'oct': 31,
-        'nov': 30,
-        'dec': 31
-    }
-
     for i in month_dict:
         if i == item:
             print('Number of days in', item.title(), 'is', month_dict[month])
